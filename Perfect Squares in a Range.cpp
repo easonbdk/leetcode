3befffbea027bcaf2c76ba9cb56a1793f2db3850
Easonbdk <46048179+easonbdk@users.noreply.github.com>
2019-11-05 09:37:15 +0800

    Update Perfect Squares in a Range.cpp

--- Perfect Squares in a Range.cpp
+++ Perfect Squares in a Range.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <cmath>
-using namespace std;
+using namespace std; 
 
-int countsquares(int a, int b){
-    return (floor(sqrt(b)) - ceil(sqrt(a)) + 1);
-}
-
-int main() {
-	//code
-	int t=1, a, b;
-	for (int i = 0; i < t; i++){
-	    cin >> a >> b;
-        cout << countsquares(a, b);
-	}
+int countSquares(int a, int b) 
+{ 
+    return (floor(sqrt(b)) - ceil(sqrt(a)) + 1); 
+} 
+  
+int main() 
+{ 
+    int t, a, b;
+    cin >> t;
+    for (int i=0; i<t; i++){
+        cin >> a >> b;
+        cout << countSquares(a, b) << endl; 
+    }
     return 0;
-}
+} 
